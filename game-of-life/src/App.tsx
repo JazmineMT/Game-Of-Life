e98906e5a80cec0794c937ea4c696a06ddaafc0c
JazmineMT <jazmineamcginnis@gmail.com>
2020-09-22 12:44:38 -0700

    working except gen counter

--- game-of-life/src/App.tsx
+++ game-of-life/src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useCallback, useState, useRef } from 'react';
+import React, { useCallback, useState, useRef , useEffect} from 'react';
 import produce from 'immer';
 
-const numRows = 50
-const numCols = 50
+
+const numRows = 25
+const numCols = 25
 
 const operations = [
   [0,1],
@@ -14,6 +15,10 @@ const operations = [
   [-1, 0],
   [-1, 1]
 ]
+
+const color = {
+  color : '	#000000'
+}
 
 const generateEmptyGrid = () => {
 
@@ -27,29 +32,36 @@ const generateEmptyGrid = () => {
 }
 
 
-const App: React.FC = () => {
+
+
+function App(){
 
   const [grid , setGrid] = useState( () => {
 
     return  generateEmptyGrid()
     })
-    
+  const [gen, setGen] = useState(0)  
   const [running , setRunning] = useState(false)
+  const [colors , setColors] = useState(color.color)
   
   const runningRef = useRef(running);
 
    runningRef.current = running
   
+  
   const runSimulation = useCallback(()=> {
+
+  
     if (!runningRef.current){
       return ;
     }
     // simulate 
     setGrid( (g) => {
       return produce((g), gridCopy => {
+        console.log('grid copy', g)
         for(let i = 0; i < numRows; i++){
           for(let k = 0; k< numCols; k++){
-            let neighbors = 0 
+            let neighbors = 0
             operations.forEach(([x, y]) => {
               const newI = i+ x;
               const newK = k + y;
@@ -62,6 +74,7 @@ const App: React.FC = () => {
               gridCopy[i][k] = 0;
 
             } else if(g[i][k]=== 0 && neighbors === 3){
+              setGen(gen+1)
               gridCopy[i][k] = 1;
             }
     
@@ -71,15 +84,132 @@ const App: React.FC = () => {
       })
 
     })
-    
-  
 
     setTimeout(runSimulation, 1000);
-
+    
   }, [])
 
 
+  const generateGlider = () => {
+  
+    setGrid( (g) => {
+      return produce((g), gridCopy => {
+        console.log('grid copy', g)
+        for(let i = 0; i < numRows; i++){
+          for(let k = 0; k< numCols; k++){
+            if( i === 1 && k === 1){
+              gridCopy[i][k] = 1
+            }else if( i === 2 && k === 2){
+              gridCopy[i][k] = 1
+            }else if( i === 3 && k === 2){
+              gridCopy[i][k] = 1
+            }else if( i === 2 && k === 3){
+              gridCopy[i][k] = 1
+            }else if( i === 1 && k === 3){
+              gridCopy[i][k] = 1
+            }else{
+              gridCopy[i][k] = 0
 
+            }
+          }
+        }
+  
+      })
+  
+    })
+  
+  
+  }
+
+
+  
+  const generateToad = () => {
+  
+    setGrid( (g) => {
+      return produce((g), gridCopy => {
+        console.log('grid copy', g)
+        for(let i = 0; i < numRows; i++){
+          for(let k = 0; k< numCols; k++){
+            if( i === 10 && k === 10){
+              gridCopy[i][k] = 1
+            }else if( i === 10 && k === 11){
+              gridCopy[i][k] = 1
+            }else if( i === 10 && k === 12){
+              gridCopy[i][k] = 1
+            }else if( i === 11 && k === 9 ){
+              gridCopy[i][k] = 1
+            }else if( i === 11 && k === 10){
+              gridCopy[i][k] = 1
+            }else if( i === 11 && k === 11){
+              gridCopy[i][k] = 1
+            }else{
+              gridCopy[i][k] = 0
+
+            }
+          }
+        }
+  
+      })
+  
+    })
+  
+  
+  }
+
+  const generatePentadecathlon = () => {
+  
+    setGrid( (g) => {
+      return produce((g), gridCopy => {
+        console.log('grid copy', g)
+        for(let i = 0; i < numRows; i++){
+          for(let k = 0; k< numCols; k++){
+            if( i === 10 && k === 10){
+              gridCopy[i][k] = 1
+            }else if( i === 11 && k === 10){
+              gridCopy[i][k] = 1
+            }else if( i === 12 && k === 9){
+              gridCopy[i][k] = 1
+            }else if( i === 12 && k === 11 ){
+              gridCopy[i][k] = 1
+            }else if( i === 13 && k === 10){
+              gridCopy[i][k] = 1
+            }else if( i === 14 && k === 10){
+            gridCopy[i][k] = 1
+          }else if( i === 15 && k === 10){
+            gridCopy[i][k] = 1
+          }else if( i === 16 && k === 9){
+            gridCopy[i][k] = 1
+          }else if( i === 16 && k === 11){
+            gridCopy[i][k] = 1
+          }else if( i === 17 && k === 10){
+            gridCopy[i][k] = 1
+          }else if( i === 18 && k === 10){
+            gridCopy[i][k] = 1
+          }else{
+              gridCopy[i][k] = 0
+
+            }
+          }
+        }
+  
+      })
+  
+    })
+  
+  
+  }
+
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  console.log(getRandomColor())
+
+     
 
   return (
 
@@ -90,39 +220,68 @@ const App: React.FC = () => {
         runSimulation()
       }}> {running? "stop": "start" }</button>
 
-
+    
       <button onClick={() => {
+        setColors('#000000')
+        setGen(0)
         setGrid(generateEmptyGrid)
 
       }}> Clear</button>
 
+      <button 
+      onClick={() => {
+        setColors(getRandomColor())
+      }}>
+        Random Color 
+      </button>
+
       <button onClick={() => {
          const rows = [];
          for( let i = 0; i < numRows; i++){
-           rows.push(Array.from(Array(numCols), () => Math.random() > .5 ? 1: 0))
+           rows.push(Array.from(Array(numCols), () => Math.random() > .7 ? 1: 0))
        
            }  
            setGrid(rows)
        
       }}> Random </button>
+
+      <button onClick={() =>{
+        generateGlider()
+      }}>Glider</button>
+
+      <button onClick={() =>{
+        generateToad()
+      }}>Toad</button>
+
+
+      <button onClick={() =>{
+        generatePentadecathlon()
+      }}>Pentadecathlon</button>
+
+
+    <div> Generation : {gen}</div>
+
+
     
 
       <div style={{
-        display:'grid',
+        display:'inline-grid',
         gridTemplateColumns: `repeat(${numCols}, 20px)`
       }}> {grid.map((rows, i) => 
               rows.map((col , k ) => 
               <div
               onClick={ () => {
-                const newGrid = produce(grid, gridCopy => {
-                  gridCopy[i][k] = grid[i][k] ?  0 : 1;
-                })
-                  setGrid(newGrid)
-              }}
-              key={`${i}-${k}`}
-              style={{width:20, height: 20,
-              backgroundColor: grid[i][k] ? 'purple' : undefined, 
-              border: "solid 1px black" }}   />))} 
+                if(!running){
+                    const newGrid = produce(grid, gridCopy => {
+                      gridCopy[i][k] = grid[i][k] ?  0 : 1;
+                    })
+                      setGrid(newGrid)
+                  }}}
+                  key={`${i}-${k}`}
+                  style={{width:20, height: 20,
+                  backgroundColor: grid[i][k] ? `${colors}` : undefined, 
+                  border: "solid 1px black",
+                  borderRadius: '0px' }}   />))} 
           </div>
     </>
     )
